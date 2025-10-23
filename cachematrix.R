f9707c1b72b6d35ab3d5ad76e9b9bdf96460bf5c
muzakkir2024 <rmuzakkir2024@gmail.com>
2025-10-23 19:35:53 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-# The makeCacheMatrix method creates a matrix object with the following funtions
-#   1. Set the value of the matrix
-#   2. Get the value of the matrix
-#   3. Set the value of the matrix inverse
-#   4. Get the value of the matrix inverse
-
-makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinv <- function(inverse) inv <<- inverse
-    getinv <- function() inv
-    list(set = set, get = get, setinv = setinv, getinv = getinv)
+setwd('C:/Users/rubind1/Documents/Coursera-R')
+##
+## I simply set the input x as a matrix
+## and then set the solved value "s" as a null
+## then I changed every reference to "mean" to "solve"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-# The cacheSolve method returns the inverse of a matrix object if it has been assigned to the object.  If the 
-# inverse has not been calculated/defined, the function will calculate and set the inverse matrix and store it
-# the matrix object.
-
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
 cacheSolve <- function(x, ...) {
-      inv <- x$getinv()
-      if(!is.null(inv)){
-          message("retrieving inverse matrix")
-          return(inv)
-      }
-      data <- x$get()
-      inv <- solve(data, ...)
-      x$setinv(inv)
-      return(inv)
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
